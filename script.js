d84cea12d6ea9afecbb6563a39ff245868fc5839
Wilord20 <edgartorres08@live.com.mx>
2024-06-17 16:35:15 -0600

    Load and Pause

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-$(function() {
+$(function () {
     $('body').terminal({
         // La funcion 'hello' requiere de una variable, en este caso 'what'
-        hello: function(what) {
+        hello: function (what) {
             this.echo('Hello, ' + what +
-                      '. Welcome to this terminal.');
+                '. Welcome to this terminal.');
         },
         // Aquí 'cat' es una funcion que no necesita de una variable y solo regresará una imagen
-        cat: function(width, height) {
+        cat: function (width, height) {
             const img = $('<img src="https://placekitten.com/' +
-                          width + '/' + height + '">');
+                width + '/' + height + '">', { raw: true });
+        // Añadimos un load y pause en caso de conexiones lentas
+            img.on('load', this.resume);
+            this.pause();
             this.echo(img);
         }
     }, {
