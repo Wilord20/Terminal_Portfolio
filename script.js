c687d6d1f599939dfdc8279c369b1158fd24804f
Wilord20 <edgartorres08@live.com.mx>
2024-06-17 16:29:16 -0600

    Organizing functions

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 $(function() {
     $('body').terminal({
+        // La funcion 'hello' requiere de una variable, en este caso 'what'
         hello: function(what) {
             this.echo('Hello, ' + what +
-                      '. Wellcome to this terminal.');
+                      '. Welcome to this terminal.');
+        },
+        // Aquí 'cat' es una funcion que no necesita de una variable y solo regresará una imagen
+        cat: function(width, height) {
+            const img = $('<img src="https://placekitten.com/' +
+                          width + '/' + height + '">');
+            this.echo(img);
         }
     }, {
         greetings: 'My First Web Terminal'
     });
 });
-
