5e0d1f667131996c6d10ac6cf2dcb7c30f3cedc3
Wilord20 <edgartorres08@live.com.mx>
2024-06-18 17:27:14 -0600

    Add random seed

--- script.js
+++ script.js
@@ -12,32 +12,29 @@ const commands = {
     term.echo(`List of available commands: ${help}`);
   },
   echo(...args) {
-    term.echo(args.join(" "));
-  },
+      term.echo(args.join(' '));
+  }
 };
 
-const command_list = ["clear"].concat(Object.keys(commands));
-const help = formatter.format(command_list);
+const command_list = ['clear'].concat(Object.keys(commands));
+const formatted_list = command_list.map(cmd => {
+  return `<white class="command">${cmd}</white>`;
+});
 
+const help = formatter.format(formatted_list);
 const term = $("body").terminal(commands, {
   greetings: false,
   checkArity: false,
-  exit: false,
+  exit: false
 });
 
-function ready() {
-  term
-    .echo(() => rainbow(render("Terminal Portfolio")))
-    .echo(
-      <white>
-        {args.map((text) => {
-          return <>{text} </>;
-        })}
-      </white>
-    )
-    .resume();
-}
+term.on('click', '.command', function() {
+  const command = $(this).text();
+  term.exec(command);
+});
 
+
+// Colors and renders
 function render(text) {
   const cols = term.cols();
   return figlet.textSync(text, {
@@ -47,13 +44,21 @@ function render(text) {
   });
 }
 
-function rainbow(string) {
-  return lolcat
-    .rainbow(function (char, color) {
+function rand(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function ready() {
+ const seed = rand(256);
+ term.echo(() => rainbow(render('Terminal Portfolio'), seed))
+     .echo('Welcome to my Terminal Portfolio\n').resume();
+}
+
+function rainbow(string, seed) {
+  return lolcat.rainbow(function(char, color) {
       char = $.terminal.escape_brackets(char);
       return `[[;${hex(color)};]${char}]`;
-    }, string)
-    .join("\n");
+  }, string, seed).join('\n');
 }
 
 function hex(color) {
