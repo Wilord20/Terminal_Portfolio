7a0aef8a4d9ec8b1f8e551b43efed2b2e518b5ee
Wilord20 <edgartorres08@live.com.mx>
2024-06-17 19:09:05 -0600

    setting up figlet

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+const font = 'Slant';
+
+figlet.defaults({ fontPath: 'https://unpkg.com/figlet/fonts/' });
+figlet.preloadFonts([font], ready);
+
+let term;
+
+function ready() {
+   term =  $('body').terminal(commands, {
+      greetings
+   });
+}
+
 $(function () {
     $('body').terminal({
         // La funcion 'hello' requiere de una variable, en este caso 'what'
         hello: function (what) {
-            this.echo('Hello, ' + what +
+            this.echo('[[b;red;white]Hello, ' + what +
                 '. Welcome to this terminal.');
         },
         // Aquí 'cat' es una funcion que no necesita de una variable y solo regresará una imagen
@@ -14,7 +27,5 @@ $(function () {
             this.pause();
             this.echo(img);
         }
-    }, {
-        greetings: greetings.innerHTML
     });
 });
